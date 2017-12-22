47c48ef64c4d3ef3d611b5bd9f350d4ceed51434
Yoggdrasil <Yoggdrasil@yandex.ru>
2017-12-22 22:58:48 +0300

    added source URL for this example

--- src/prompt/Main.java
+++ src/prompt/Main.java
@@ -2,6 +2,7 @@ package prompt;
 
 public class Main {
 
+    // https://docs.oracle.com/javase/tutorial/java/IandI/override.html
     public static void main(String[] args) {
         Cat myCat = new Cat();  // экземпляр наследника
         Animal myAnimal = myCat;  // смотрим на наследника через призму родителя
